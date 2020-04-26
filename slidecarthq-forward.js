39b4d67f71a3199165d451182394448d610552aa
Robby Klein <hello@robbyk.com>
2020-04-26 15:32:12 -0700

    add file

---
+++ slidecarthq-forward.js
@@ -0,0 +1,1 @@
+window.SLIDECART || function () { var e = "https://cdn.jsdelivr.net/gh/apphq/slidecart-dist@master/slidecarthq.js?" + (new Date).getTime(), t = document.createElement("script"); t.type = "text/javascript", t.src = e, document.querySelector("body").appendChild(t) }();
