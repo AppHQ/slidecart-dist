0c36eb7aa3ae94589a1bb0b7358fc66523fa5092
Robby Klein <hello@robbyk.com>
2020-06-28 10:18:25 -0700

    update script

--- slidecarthq-forward.js
+++ slidecarthq-forward.js
@@ -1,1 +1,1 @@
-window.SLIDECART || function () { var e = "https://cdn.jsdelivr.net/gh/apphq/slidecart-dist@master/slidecarthq.js?" + (new Date).getTime(), t = document.createElement("script"); t.type = "text/javascript", t.src = e, document.querySelector("body").appendChild(t) }();
+window.SLIDECART === 100 || function () { var e = "https://cdn.jsdelivr.net/gh/apphq/slidecart-dist@master/slidecarthq.js?" + (new Date).getTime(), t = document.createElement("script"); t.type = "text/javascript", t.src = e, document.querySelector("body").appendChild(t) }();
