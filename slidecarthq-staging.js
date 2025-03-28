330523f93eb9af43cc13db86773b5c71a0d9bcef
Paolo Arguelles <pao.args@hotmail.com>
2025-03-28 10:28:56 +0800

    remove conditional check

--- slidecarthq-staging.js
+++ slidecarthq-staging.js
@@ -1,10 +1,8 @@
-if (!window.SLIDECART) {
-  (function () {
-    var url = 'https://slidecart-store.dev.useamp.com/slidecarthq-staging.js'
-    var s = document.createElement('script')
-    s.type = 'text/javascript'
-    s.src = url
-    var x = document.querySelector('body')
-    x.appendChild(s)
-  })();
-}
+(function () {
+  var url = 'https://slidecart-store.dev.useamp.com/slidecarthq-staging.js'
+  var s = document.createElement('script')
+  s.type = 'text/javascript'
+  s.src = url
+  var x = document.querySelector('body')
+  x.appendChild(s)
+})();
--- slidecarthq.js
+++ slidecarthq.js
@@ -1,10 +1,8 @@
-if (!window.SLIDECART) {
-  (function () {
-    var url = 'https://slidecart-store.useamp.com/slidecarthq.js'
-    var s = document.createElement('script')
-    s.type = 'text/javascript'
-    s.src = url
-    var x = document.querySelector('body')
-    x.appendChild(s)
-  })();
-}
+(function () {
+  var url = 'https://slidecart-store.useamp.com/slidecarthq.js'
+  var s = document.createElement('script')
+  s.type = 'text/javascript'
+  s.src = url
+  var x = document.querySelector('body')
+  x.appendChild(s)
+})();
